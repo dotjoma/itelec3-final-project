3eb9bea4e5433112fa28df94dc345e66a1a94438
dotjoma <bastejoma8@gmail.com>
2025-03-12 06:19:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./public/favicon.ico">
     <link rel="stylesheet" href="./resources/css/index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>PC Worth | PC Worth</title>
 </head>
 <body>
-    <div class="muibox">
-        <div class="marquee-container">
-            <div class="marquee">
-                PC Worth offers same day delivery on Metro Manila and nearby areas of
-                Rizal, Cavite and Bulacan. Express Delivery (Same Day) orders can be placed until 3:00 PM only.
-            </div>
-        </div>
-    </div>
+    <!-- Start of Marqueetop -->
+    <?php require_once("./includes/marqueetop.php"); ?>
+    <!-- End of Marqueetop -->
+
+    <!-- Start of Header -->
+    <?php require_once("./includes/header.php"); ?>
+    <!-- End of Header -->
+
+    <!-- Start of Navbar -->
+    <?php require_once("./includes/nav.php"); ?>
+    <!-- End of Navbar -->
+
+    <!-- Start of Nav Brand -->
+    <?php require_once("./includes/navbrand.php"); ?>
+    <!-- End of Nav Brand -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./resources/js/index.js"></script>
 </body>
 </html>
