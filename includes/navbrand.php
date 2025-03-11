be597573e904b80bf2f03ec3b8fa0ec37ae2e18a
dotjoma <bastejoma8@gmail.com>
2025-03-12 06:18:01 +0800

    Update navbrand.php

---
+++ includes/navbrand.php
@@ -0,0 +1,16 @@
+<div class="navbrand">
+    <div class="container d-flex justify-content-center align-items-center gap-3">
+        <a href="#">
+            <img src="./resources/images/gigabyte2025.webp" alt="Gigabyte" class="brand-logo">
+        </a>
+        <a href="#">
+            <img src="./resources/images/asus2025.webp" alt="Asus" class="brand-logo">
+        </a>
+        <a href="#">
+            <img src="./resources/images/asrock2025.webp" alt="ASRock" class="brand-logo">
+        </a>
+        <a href="#">
+            <img src="./resources/images/msi2025.webp" alt="MSI" class="brand-logo">
+        </a>
+    </div>
+</div>
