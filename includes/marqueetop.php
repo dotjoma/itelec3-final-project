8bbb87935a6caba1bff2002d80fbdff42b65bb1b
dotjoma <bastejoma8@gmail.com>
2025-03-12 06:17:33 +0800

    Update marqueetop.php

---
+++ includes/marqueetop.php
@@ -0,0 +1,13 @@
+<div class="muibox">
+    <div class="marquee-container">
+        <div class="marquee">
+            PC Worth offers same day delivery on Metro Manila and nearby areas of
+            Rizal, Cavite and Bulacan. Express Delivery (Same Day) orders can be placed until 3:00 PM only.
+        </div>
+
+        <div class="marquee">
+            PC Worth offers same day delivery on Metro Manila and nearby areas of
+            Rizal, Cavite and Bulacan. Express Delivery (Same Day) orders can be placed until 3:00 PM only.
+        </div>
+    </div>
+</div>
