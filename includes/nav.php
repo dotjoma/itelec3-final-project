b0ec8ca262b0c511e2a86a53e89eb0dd256a4a4c
dotjoma <bastejoma8@gmail.com>
2025-03-12 06:17:48 +0800

    Update nav.php

---
+++ includes/nav.php
@@ -0,0 +1,196 @@
+<nav class="navbar windows-navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <!-- Navbar Content -->
+        <div class="collapse navbar-collapse justify-content-center">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-1" href="#">
+                        <svg width="25" height="25" viewBox="0 0 24 24">
+                            <path d="M20 9V7c0-1.1-.9-2-2-2h-3c0-1.66-1.34-3-3-3S9 3.34 9 5H6c-1.1 0-2 .9-2 2v2c-1.66 0-3 1.34-3 3s1.34 3 3 3v4c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-4c1.66 0 3-1.34 3-3s-1.34-3-3-3zm-2 10H6V7h12v12zm-9-6c-.83 0-1.5-.67-1.5-1.5S8.17 10 9 10s1.5.67 1.5 1.5S9.83 13 9 13zm7.5-1.5c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5 1.5.67 1.5 1.5zM8 15h8v2H8v-2z"></path>
+                        </svg>
+                        <span class="fw-semibold">AI Builder</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-1" href="#">
+                        <svg width="25" height="25" viewBox="0 0 24 24">
+                            <path d="M21 2H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7v2H8v2h8v-2h-2v-2h7c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H3V4h18v12z"></path>
+                        </svg>
+                        <span class="fw-semibold">PC Builder</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-1" href="#">
+                        <svg width="25" height="25" viewBox="0 0 24 24">
+                            <path d="M20 4v12H8V4h12m0-2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 9.67 1.69 2.26 2.48-3.1L19 15H9zM2 6v14c0 1.1.9 2 2 2h14v-2H4V6H2z"></path>
+                        </svg>
+                        <span class="fw-semibold">PC Gallery</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-1" href="#">
+                        <svg width="25" height="25" viewBox="0 0 24 24">
+                            <path d="M11 7h6v2h-6zm0 4h6v2h-6zm0 4h6v2h-6zM7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7zM20.1 3H3.9c-.5 0-.9.4-.9.9v16.2c0 .4.4.9.9.9h16.2c.4 0 .9-.5.9-.9V3.9c0-.5-.5-.9-.9-.9zM19 19H5V5h14v14z"></path>
+                        </svg>
+                        <span class="fw-semibold">Pricelist</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-1" href="#">
+                        <img width="25" height="25" src="./resources/images/Frame.webp" alt="">
+                        <span class="fw-semibold">Blogs & Articles</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-1" href="#">
+                        <img width="25" height="20" src="./resources/images/powered_builds.webp" alt="">
+                        <span class="fw-semibold">Powered Builds</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-1" href="#">
+                        <img width="25" height="20" src="./resources/images/promo.webp" alt="">
+                        <span class="fw-semibold">Promos</span>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center gap-1" href="#">
+                        <img width="25" height="25" src="./resources/images/pc-worth-extra-service-seal.webp" alt="">
+                        <span class="fw-semibold">PC Worth Extra Service</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Mobile/Tablet Navigation (below 988px) -->
+<nav class="navbar fixed-bottom d-lg-none bg-white border-top">
+    <div class="container-fluid px-0">
+        <ul class="navbar-nav w-100 d-flex flex-row justify-content-around">
+            <!-- First 5 important items -->
+            <li class="nav-item text-center">
+                <a class="nav-link d-flex flex-column align-items-center" href="#">
+                    <svg width="20" height="20" viewBox="0 0 24 24">
+                        <path d="M20 9V7c0-1.1-.9-2-2-2h-3c0-1.66-1.34-3-3-3S9 3.34 9 5H6c-1.1 0-2 .9-2 2v2c-1.66 0-3 1.34-3 3s1.34 3 3 3v4c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-4c1.66 0 3-1.34 3-3s-1.34-3-3-3z"></path>
+                    </svg>
+                    <small>AI Builder</small>
+                </a>
+            </li>
+            <li class="nav-item text-center">
+                <a class="nav-link d-flex flex-column align-items-center" href="#">
+                    <svg width="20" height="20" viewBox="0 0 24 24">
+                        <path d="M21 2H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7v2H8v2h8v-2h-2v-2h7c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"></path>
+                    </svg>
+                    <small>PC Builder</small>
+                </a>
+            </li>
+            <li class="nav-item text-center">
+                <a class="nav-link d-flex flex-column align-items-center" href="#">
+                    <svg width="20" height="20" viewBox="0 0 24 24">
+                        <path d="M20 4v12H8V4h12m0-2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"></path>
+                    </svg>
+                    <small>PC Gallery</small>
+                </a>
+            </li>
+            <li class="nav-item text-center">
+                <a class="nav-link d-flex flex-column align-items-center" href="#">
+                    <svg width="20" height="20" viewBox="0 0 24 24">
+                        <path d="M11 7h6v2h-6zm0 4h6v2h-6zm0 4h6v2h-6zM7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7z"></path>
+                    </svg>
+                    <small>Pricelist</small>
+                </a>
+            </li>
+            <li class="nav-item text-center">
+                <button class="nav-link d-flex flex-column align-items-center border-0 bg-transparent" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileSidebar">
+                    <svg width="20" height="20" viewBox="0 0 24 24">
+                        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+                    </svg>
+                    <small>Menu</small>
+                </button>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!-- Sidebar Menu -->
+<div class="offcanvas offcanvas-end offcanvas-custom" tabindex="-1" id="mobileSidebar">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title">All Menu</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+    </div>
+    <div class="offcanvas-body">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-1" href="#">
+                    <svg width="25" height="25" viewBox="0 0 24 24">
+                        <path d="M20 9V7c0-1.1-.9-2-2-2h-3c0-1.66-1.34-3-3-3S9 3.34 9 5H6c-1.1 0-2 .9-2 2v2c-1.66 0-3 1.34-3 3s1.34 3 3 3v4c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-4c1.66 0 3-1.34 3-3s-1.34-3-3-3zm-2 10H6V7h12v12zm-9-6c-.83 0-1.5-.67-1.5-1.5S8.17 10 9 10s1.5.67 1.5 1.5S9.83 13 9 13zm7.5-1.5c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5 1.5.67 1.5 1.5zM8 15h8v2H8v-2z"></path>
+                    </svg>
+                    <span class="fw-semibold">AI Builder</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-1" href="#">
+                    <svg width="25" height="25" viewBox="0 0 24 24">
+                        <path d="M21 2H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7v2H8v2h8v-2h-2v-2h7c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H3V4h18v12z"></path>
+                    </svg>
+                    <span class="fw-semibold">PC Builder</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-1" href="#">
+                    <svg width="25" height="25" viewBox="0 0 24 24">
+                        <path d="M20 4v12H8V4h12m0-2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 9.67 1.69 2.26 2.48-3.1L19 15H9zM2 6v14c0 1.1.9 2 2 2h14v-2H4V6H2z"></path>
+                    </svg>
+                    <span class="fw-semibold">PC Gallery</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-1" href="#">
+                    <svg width="25" height="25" viewBox="0 0 24 24">
+                        <path d="M11 7h6v2h-6zm0 4h6v2h-6zm0 4h6v2h-6zM7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7zM20.1 3H3.9c-.5 0-.9.4-.9.9v16.2c0 .4.4.9.9.9h16.2c.4 0 .9-.5.9-.9V3.9c0-.5-.5-.9-.9-.9zM19 19H5V5h14v14z"></path>
+                    </svg>
+                    <span class="fw-semibold">Pricelist</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-1" href="#">
+                    <img width="25" height="25" src="./resources/images/Frame.webp" alt="">
+                    <span class="fw-semibold">Blogs & Articles</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-1" href="#">
+                    <img width="25" height="20" src="./resources/images/powered_builds.webp" alt="">
+                    <span class="fw-semibold">Powered Builds</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-1" href="#">
+                    <img width="25" height="20" src="./resources/images/promo.webp" alt="">
+                    <span class="fw-semibold">Promos</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-1" href="#">
+                    <img width="25" height="25" src="./resources/images/pc-worth-extra-service-seal.webp" alt="">
+                    <span class="fw-semibold">PC Worth Extra Service</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
