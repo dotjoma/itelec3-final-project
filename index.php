08a8d30ca4397fbd6c28a613fce2451ed24043f6
dotjoma <bastejoma8@gmail.com>
2025-03-11 21:57:45 +0800

    My Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./public/favicon.ico">
+    <link rel="stylesheet" href="./resources/css/index.css">
+    <title>PC Worth | PC Worth</title>
+</head>
+<body>
+    <div class="muibox">
+        <div class="marquee-container">
+            <div class="marquee">
+                PC Worth offers same day delivery on Metro Manila and nearby areas of
+                Rizal, Cavite and Bulacan. Express Delivery (Same Day) orders can be placed until 3:00 PM only.
+            </div>
+        </div>
+    </div>
+</body>
+</html>
